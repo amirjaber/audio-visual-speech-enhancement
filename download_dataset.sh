ae6678287c53e6bf323988a8912c5089d42c6cac
amirjaber <amirhossein.jaberzadeh@gmail.com>
2020-01-29 09:42:43 -0800

    data divided into train validation and test

--- download_dataset.sh
+++ download_dataset.sh
@@ -1,25 +1,32 @@
 #preparing for download 
 mkdir "gridcorpus"
 cd "gridcorpus"
-mkdir "raw"
+mkdir "raw" "train" "validation" "test"
 cd "raw" && mkdir "audio" "video"
 
-for i in `seq $1 $2`
+for i in `seq 1 $1`
 do
     printf "\n\n------------------------- Downloading $i th speaker -------------------------\n\n"
     
     #download the audio of the ith speaker
     cd "audio" && curl "http://spandh.dcs.shef.ac.uk/gridcorpus/s$i/audio/s$i.tar" > "s$i.tar" && cd ..
     cd "video" && curl "http://spandh.dcs.shef.ac.uk/gridcorpus/s$i/video/s$i.mpg_vcd.zip" > "s$i.zip" && cd ..
+ 
+        mkdir "../train/s$i" "../train/s$i/video" "../train/s$i/audio"         
+        mkdir "../validation/s$i" "../validation/s$i/video" "../validation/s$i/audio"
+        mkdir "../test/s$i" "../test/s$i/video" "../test/s$i/audio"
 
-    if (( $3 == "y" ))
-    printf "\n\n--untar the files--\n\n"
-    then
-        mkdir "../s$i"
-        mkdir "../s$i/video" "../s$i/audio"
+        mkdir "../s$i/" "../s$i/video" "../s$i/audio"
         unzip -q "video/s$i.zip" -d "../s$i/video"
-        mv ../s$i/video/s$i/* ../s$i/video/ && rm -rf ../s$i/video/s$i        
         tar -xf "audio/s$i.tar" -C "../s$i/audio"
-        mv ../s$i/audio/s$i/* ../s$i/audio/ && rm -rf ../s$i/audio/s$i
-    fi
+
+        mv ../s$i/video/s$i/bb* ../train/s$i/video/        
+        mv ../s$i/audio/s$i/bb* ../train/s$i/audio/
+
+        mv ../s$i/video/s$i/pb* ../validation/s$i/video/       
+        mv ../s$i/audio/s$i/pb* ../validation/s$i/audio/
+
+        mv ../s$i/video/s$i/sb* ../test/s$i/video/        
+        mv ../s$i/audio/s$i/sb* ../test/s$i/audio/ && rm -rf ../s$i
 done
+rm -rf ../raw/
